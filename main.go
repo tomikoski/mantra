b3fd1d81a6edbfe1199cebe6928363b94139f061
Tomi (tk0) Koski <1329786+tomikoski@users.noreply.github.com>
2024-09-02 14:15:41 +0300

    ep flag verbose

--- main.go
+++ main.go
@@ -89,7 +89,11 @@ func req(url string) {
 					}
 				}
 			} else {
-				fmt.Println("\033[32m[+]\033[37m", url, "\033[32m[\033[37m"+secret+"\033[32m]\033[37m")
+				if secret != *extrapattern {
+					fmt.Println("\033[32m[+]\033[37m", url, "\033[32m[\033[37m"+secret+"\033[32m]\033[37m")
+				} else {
+					fmt.Println("\033[33m[+]\033[37m", url, "\033[33m[\033[37m"+secret+"\033[33m]\033[37m Extra pattern detected!")
+				}
 			}
 		}
 	}
